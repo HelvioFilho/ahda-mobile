8701f2e26b6748867c45fc81035c38e0826b4d52
Hélvio Filho <helviosvf@gmail.com>
2022-03-20 10:16:53 -0300

    provisional colors

--- src/global/styles/theme.ts
+++ src/global/styles/theme.ts
@@ -2,7 +2,17 @@ import { metrics } from './metrics';
 
 export default {
   colors: {
+    text: '#0F0F0F',
+    text_mid: '#3E3E3E',
+    text_light: '#CFCFCF',
 
+    background: '#FFFFFF',
+
+    neutral: '#4B92D4',
+
+    success: '#4DCFC0',
+    warning: '#EEA63A',
+    error: '#e52b67',
   },
   fonts: {
 
