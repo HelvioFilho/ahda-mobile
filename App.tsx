6fb1fb77157448a123e5950549688fba3cef5ba0
Hélvio Filho <helviosvf@gmail.com>
2022-04-23 22:24:46 -0300

    player on

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Text, View } from 'react-native';
 import { ThemeProvider } from 'styled-components';
+import { Player } from './src/components/Player';
 import theme from './src/global/styles/theme';
 import { SinglePage } from './src/screens/SinglePage';
 
@@ -8,7 +9,8 @@ import { SinglePage } from './src/screens/SinglePage';
 export default function App() {
   return (
     <ThemeProvider theme={theme}>
-      <SinglePage />
+      {/* <SinglePage /> */}
+      <Player />
     </ThemeProvider>
   );
 }
---
+++ src/components/Player/index.tsx
@@ -0,0 +1,54 @@
+import { Audio } from 'expo-av';
+import React, { useEffect, useState } from 'react';
+import { Button, View } from 'react-native';
+
+import { Container } from './styles';
+
+export function Player() {
+  const urlPlayer = "https://s18.maxcast.com.br:8707/live";
+  const [sound, setSound] = useState();
+  const [playing, setPlaying] = useState(false);
+
+  const source = { uri: "https://s18.maxcast.com.br:8707/live" }
+  const initialStatus = { shouldPlay: playing }
+
+  async function playSound() {
+    console.log('Loading Sound');
+    Audio.setAudioModeAsync(
+      {
+        interruptionModeIOS: Audio.INTERRUPTION_MODE_IOS_DO_NOT_MIX,
+        playsInSilentModeIOS: true,
+        interruptionModeAndroid: Audio.INTERRUPTION_MODE_ANDROID_DUCK_OTHERS,
+        shouldDuckAndroid: true,
+        staysActiveInBackground: true,
+        playThroughEarpieceAndroid: true,
+      }
+    )
+    const { sound } = await Audio.Sound.createAsync(source, initialStatus);
+    setSound(sound);
+
+    console.log('Playing Sound');
+    await sound.playAsync();
+  }
+
+  useEffect(() => {
+    return sound
+      ? () => {
+        console.log('Unloading Sound');
+        sound.unloadAsync();
+      }
+      : undefined;
+  }, [sound]);
+
+  useEffect(() => {
+    playing && playSound();
+  }, []);
+
+  return (
+    <Container>
+      <View>
+        <Button title="Play Sound" onPress={playSound} />
+      </View>
+    </Container>
+  );
+}
